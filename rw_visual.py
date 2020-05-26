28c451575315e1d6fa30118ec6659bc4014e4f09
CutestHamster <65850137+CutestHamster@users.noreply.github.com>
2020-05-26 05:34:26 -0400

    Changing number of points and DPI

--- rw_visual.py
+++ rw_visual.py
@@ -5,15 +5,15 @@ from random_walk import RandomWalk
 # Keep making new walks as long as the program is active
 while True:
     # Make a random walk
-    rw = RandomWalk()
+    rw = RandomWalk(100_000)
     rw.fill_walk()
 
     # Plot the points in the walk
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 5), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
-        edgecolors='none', s=15)
+        edgecolors='none', s=2)
 
     # Emphasize the first and last points
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
