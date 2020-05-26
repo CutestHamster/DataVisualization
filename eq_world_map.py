bbe6ca0f628e593ff9b02ec3d51042b0a21d8347
CutestHamster <65850137+CutestHamster@users.noreply.github.com>
2020-05-26 11:44:46 -0400

    Updated with recent data and refactored code

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,35 +4,35 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data
-filename = 'data/eq_data_30_day_m1.json'
+filename = 'data/eq_data_30_day_m4.5.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
+eq_title = all_eq_data['metadata']['title']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
 # Map the earthquakes
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
-        'colorscale': 'Hot',
+        'colorscale': 'Viridis',
         'reversescale': True,
         'colorbar': {'title': 'Magnitude'}
     }
 }]
-my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title=eq_title)
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
